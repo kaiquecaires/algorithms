f24a3d637b501ff3ff3dfdce11a23374b62bf1a7
kaique coimbra <kaique.caires@yahoo.com>
2020-12-29 17:38:09 -0300

    feat: implements LinkedList

---
+++ src/algorithms/linked-list.js
@@ -0,0 +1,120 @@
+class Node {
+  constructor (data, next = null) {
+    this.data = data
+    this.next = next
+  }
+}
+
+export class LinkedList {
+  constructor () {
+    this.head = null
+    this.size = 0
+  }
+
+  // Insert first node
+  insertFirst (data) {
+    this.head = new Node(data, this.head)
+    this.size++
+  }
+
+  // Insert Last node
+  insertLast (data) {
+    const node = new Node(data)
+
+    if (!this.head) {
+      this.head = node
+    } else {
+      let current = this.head
+
+      while (current.next) {
+        current = current.next
+      }
+
+      current.next = node
+      this.size++
+    }
+  }
+
+  // Insert at index
+  insertAt (data, index) {
+    if (index > this.size || index < 0) {
+      return 'index out of range'
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head)
+      return
+    }
+
+    const node = new Node(data)
+    let current, previous
+
+    current = this.head
+    let count = 0
+
+    while (count < index) {
+      previous = current // Node before index
+      current = current.next // Node after index
+      count++
+    }
+
+    node.next = current
+    previous.next = node
+
+    this.size++
+  }
+
+  // Remove at index
+  removeAt (index) {
+    if (index > this.size || index < 0) {
+      return 'index out of range'
+    }
+
+    let count = 0
+    let current = this.head
+    let previous
+
+    while (count < index) {
+      previous = current
+      current = current.next
+      count++
+    }
+
+    previous.next = current.next
+
+    this.size--
+  }
+
+  // Get at index
+  getAt (index) {
+    if (index > this.size || index < 0) {
+      return 'index out of range'
+    }
+
+    let count = 0
+    let current = this.head
+
+    while (count < index) {
+      current = current.next
+      count++
+    }
+
+    return current.data
+  }
+
+  // Clear list
+  clear () {
+    this.size = 0
+    this.head = null
+  }
+
+  // Print list data
+  printListData () {
+    let current = this.head
+
+    while (current) {
+      console.log(current.data)
+      current = current.next
+    }
+  }
+}
