533be4698c348874972e4e869b9a906970583050
kaique coimbra <kaique.caires@yahoo.com>
2020-12-29 20:46:01 -0300

    test: ensure should returns index out of range if index does not exists

--- src/__tests__/linked-list.spec.js
+++ src/__tests__/linked-list.spec.js
@@ -20,3 +20,15 @@ test('should be able to insert a item in a first position', () => {
 
   expect(ll.getAt(0)).toBe(300)
 })
+
+test('should returns index out of range if index does not exists', () => {
+  const ll = new LinkedList()
+
+  ll.insertLast(100)
+  ll.insertLast(200)
+  ll.insertFirst(300)
+
+  expect(ll.getAt(5)).toBe('index out of range')
+  expect(ll.insertAt(1000, 5)).toBe('index out of range')
+  expect(ll.removeAt(5)).toBe('index out of range')
+})
