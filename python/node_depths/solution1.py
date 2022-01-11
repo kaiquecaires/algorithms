c64b9ceaf12b453970924060c1ba2dede571ff54
kaique caires <kaique.caires@yahoo.com>
2022-01-11 17:27:23 -0300

    feat(node_depths): added solution 1

---
+++ python/node_depths/solution1.py
@@ -0,0 +1,28 @@
+def node_depths(root):
+  arr_total = [0]
+  sum_node_depths(root, 0, arr_total)
+  return arr_total[0]
+
+def sum_node_depths(tree, depth, arr_total):
+  if tree is None:
+    return
+
+  arr_total[0] += depth
+  depth += 1
+
+  sum_node_depths(tree.left, depth, arr_total)
+  sum_node_depths(tree.right, depth, arr_total)
+
+
+class BST:
+  def __init__(self, value):
+      self.value = value
+      self.left = None
+      self.right = None
+
+
+bst = BST(10)
+bst.left = BST(8)
+bst.right = BST(12)
+
+print(node_depths(bst))
